5cb22f5e7198e0add4d9e3f7f9cb550c8912504b
Moilanen Mikael <mikaelmo@cubbli20-gold.local>
2021-09-07 17:21:06 +0300

    Add functions is_admin and no_admins

--- users.py
+++ users.py
@@ -2,11 +2,11 @@ from db import db
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask import session
 
-def create_user(username, password):
+def create_user(username, password, admin = "0"):
     hash_value = generate_password_hash(password)
     sql = "INSERT INTO users (username, password, is_admin) \
         VALUES (:username, :password, :is_admin)"
-    db.session.execute(sql, {"username":username, "password":hash_value, "is_admin":'0'})
+    db.session.execute(sql, {"username":username, "password":hash_value, "is_admin":admin})
     db.session.commit()
 
 def check_login(username, password):
@@ -21,3 +21,19 @@ def check_login(username, password):
             return True
         else:
             return False
+
+def is_admin():
+    sql = "SELECT 1 FROM users WHERE is_admin='1' and id=:user_id"
+    result = db.session.execute(sql, {"user_id":session["user_id"]})
+    admin = result.fetchone()
+    if not admin:
+        return False
+    return True
+
+def no_admins():
+    sql = "SELECT 1 FROM users WHERE is_admin='1'"
+    result = db.session.execute(sql)
+    admin = result.fetchone()
+    if not admin:
+        return True
+    return False
