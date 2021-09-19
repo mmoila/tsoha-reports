a68a10a6235c924d47d28ba49fc8ad70a038618c
Moilanen Mikael <mikaelmo@cubbli20-gold.local>
2021-09-19 20:55:00 +0300

    Add creation timestamp to reports

--- reports.py
+++ reports.py
@@ -1,15 +1,15 @@
 from db import db
 
 def get_all():
-    sql = "SELECT A.id, A.date, A.title, A.description, C.username, B.ICAO_ident \
+    sql = "SELECT A.id, A.date, A.title, A.description, C.username, B.ICAO_ident, A.created_at \
         FROM reports A LEFT JOIN locations B ON A.location_ID = B.id \
         LEFT JOIN users C ON A.user_id = C.id"
     result = db.session.execute(sql)
     return result.fetchall()
 
 def send(date, title, description, id, user_id):
-    sql = "INSERT INTO reports (date, title, description, location_id, user_id) VALUES \
-        (:date, :title, :description, :location, :user_id)"
+    sql = "INSERT INTO reports (date, title, description, location_id, user_id, created_at) VALUES \
+        (:date, :title, :description, :location, :user_id, date_trunc('minute', CURRENT_TIMESTAMP))"
     db.session.execute(
     sql, {"date":date, "title":title, "description":description, "location":id, "user_id":user_id})
     db.session.commit()
